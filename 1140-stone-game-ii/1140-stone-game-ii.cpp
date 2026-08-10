aab2ae3fec10108614e16b5e9eebf250bee2b865
Yugank Prajapati <135359136+yugank2002@users.noreply.github.com>
2026-08-10 11:28:13 +0530

    Time: 66 ms (13.46%), Space: 31.1 MB (9.65%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.cpp
+++ 1140-stone-game-ii/1140-stone-game-ii.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
-    int stoneGameII(vector<int>& piles) {
-        vector<vector<int>> memo(piles.size(), vector<int>(piles.size()));
-        vector<int> suffixSum = piles;
-        for (int i = suffixSum.size() - 2; i >= 0; --i)
-            suffixSum[i] += suffixSum[i + 1];
-        return maxStones(suffixSum, 1, 0, memo);
+int solve(int ind, int M, bool flag, vector<int>&arr, vector<vector<vector<int>>>&dp){
+    if(ind>=arr.size())return 0;
+
+    if(dp[ind][M][flag]!=-1)return dp[ind][M][flag];
+
+    int stone = 0;
+    int result = flag?INT_MIN:INT_MAX;
+    for(int x=1; x<=2*M && ind+x<=arr.size(); x++){
+        stone+=arr[ind+x-1];
+        if(flag){
+            result = max(result,stone + solve(ind+x,max(x,M),false,arr,dp));
+        }
+        else{
+            result = min(result,solve(ind+x,max(x,M),true,arr,dp));
+        }
     }
 
-    int maxStones(vector<int>& suffixSum, int maxTillNow, int currIndex,
-                  vector<vector<int>>& memo) {
-        if (currIndex + 2 * maxTillNow >= suffixSum.size())
-            return suffixSum[currIndex];
-        if (memo[currIndex][maxTillNow] > 0) return memo[currIndex][maxTillNow];
-        int res = INT_MAX;
-
-        for (int i = 1; i <= 2 * maxTillNow; ++i) {
-            res = min(res, maxStones(suffixSum, max(i, maxTillNow),
-                                     currIndex + i, memo));
-        }
-        memo[currIndex][maxTillNow] = suffixSum[currIndex] - res;
-        return memo[currIndex][maxTillNow];
+    return dp[ind][M][flag] = result;
+}
+    int stoneGameII(vector<int>& piles) {
+        int n = piles.size();
+        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(n+1, vector<int>(2,-1)));
+        return solve(0,1,1,piles,dp);
     }
 };
