9f730977d4422559b742035be90dbfea9eb6c10f
Yugank Prajapati <135359136+yugank2002@users.noreply.github.com>
2026-08-11 11:59:32 +0530

    Time: 3 ms (52.13%), Space: 54.9 MB (86.28%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -2,16 +2,8 @@ class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
         
-        int count = 0;
-        for(int i=0; i<nums.size(); i++){
-            if(nums[i]>0){
-            count++;
-
-            
-            }
-
-        }
-        cout<<count<<endl;
+       
+        
         for(int i=0; i<nums.size(); i++){
             
             while(nums[i]>=1 && nums[i]<=nums.size() && nums[i]!=nums[nums[i]-1]){
@@ -25,7 +17,7 @@ public:
         
         
         int i=0;
-        while(i<count && nums[i] == i+1 ){
+        while(i<nums.size() && nums[i] == i+1 ){
             i++;
         }
 
