2ba297b80bff70c5ef21fdcefd5072ef130579db
Yugank Prajapati <135359136+yugank2002@users.noreply.github.com>
2026-08-15 12:56:27 +0530

    Time: 0 ms (100%), Space: 10.7 MB (47.19%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
-
     int rob(vector<int>& nums) {
-        
 
-        vector<int>ans(nums.size());
-        for(int i=0; i<ans.size(); i++){
-            int take = nums[i]+ ((i-2>=0)?ans[i-2]:0);
-            int notTake = (i-1>=0)?ans[i-1]:0;
-            ans[i] = max(take,notTake);
+        vector<int> ans(nums.size());
+        for (int i = 0; i < ans.size(); i++) {
+            int take = nums[i] + ((i - 2 >= 0) ? ans[i - 2] : 0);
+            int notTake = (i - 1 >= 0) ? ans[i - 1] : 0;
+            ans[i] = max(take, notTake);
         }
 
-        return ans[ans.size()-1];
+        return ans[ans.size() - 1];
     }
 };
