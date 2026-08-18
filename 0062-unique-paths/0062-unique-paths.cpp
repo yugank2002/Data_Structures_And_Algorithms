c8a04b467d4adbc43cbbc0e8804d9ed93c35de0a
Yugank Prajapati <135359136+yugank2002@users.noreply.github.com>
2026-08-18 12:37:08 +0530

    Time: 0 ms (100%), Space: 9.4 MB (31.16%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
-int solve(int i, int j, vector<vector<int>>&memo){
-    if(i==0 && j==0)return 1;
 
-    if(memo[i][j]!=-1)return memo[i][j];
-
-    int up = (i-1<0)?0:solve(i-1,j,memo);
-    int left = (j-1<0)?0:solve(i,j-1,memo);
-
-    return memo[i][j] = up+left;
-}
     int uniquePaths(int m, int n) {
-        vector<vector<int>>memo(m,vector<int>(n,-1));
-        return solve(m-1,n-1,memo);
+        vector<vector<int>>memo(m,vector<int>(n));
+        memo[0][0] = 1;
+        for(int i=0;i<m; i++){
+            for(int j=0;j<n; j++){
+                if(i!=0 || j!=0){
+                    int up = (i-1<0)?0:memo[i-1][j];
+                    int left = (j-1<0)?0:memo[i][j-1];
+                    memo[i][j] = up+left;
+                }
+            }
+        }
+        return memo[m-1][n-1];
     }
 };
