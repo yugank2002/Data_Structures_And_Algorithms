fe3beeffc8211e1ffdb8aa3d39295354cd20b3b6
Yugank Prajapati <135359136+yugank2002@users.noreply.github.com>
2026-08-18 13:36:53 +0530

    Time: 3 ms (39.68%), Space: 15 MB (47.35%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
-int solve(int i, int j, vector<vector<int>>&mat, vector<vector<int>>&memo){
-    if(i==mat.size()-1)return mat[i][j];
-
-    if(memo[i][j]!=INT_MAX)return memo[i][j];
-
-    int left = (j-1<0)?INT_MAX:solve(i+1,j-1,mat,memo);
-    int down = solve(i+1,j,mat,memo);
-    int right = (j+1>=mat.size())?INT_MAX:solve(i+1,j+1,mat,memo);
-
-    return memo[i][j] = mat[i][j]+min({left,right,down});
-}
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        int mini = INT_MAX;
         int n = matrix.size();
-        vector<vector<int>>memo(n,vector<int>(n,INT_MAX));
-        for(int i=0; i<matrix.size(); i++){
-            mini = min(mini,solve(0,i,matrix,memo));
+        vector<vector<int>>memo(n,vector<int>(n));
+
+        for(int j=0; j<n; j++){
+            memo[0][j] = matrix[0][j];
+            
+        }
+        for(int i=1; i<n; i++){
+            for(int j=0; j<n; j++){
+                int up = memo[i-1][j];
+                int left = (j-1<0)?INT_MAX:memo[i-1][j-1];
+                int right = (j+1>=n)?INT_MAX:memo[i-1][j+1];
+
+                memo[i][j] = matrix[i][j]+min({up,left,right});
+            }
+        }
+
+        int mini = INT_MAX;
+
+        for(int j=0; j<n; j++){
+            mini = min(mini,memo[n-1][j]);
         }
 
         return mini;
