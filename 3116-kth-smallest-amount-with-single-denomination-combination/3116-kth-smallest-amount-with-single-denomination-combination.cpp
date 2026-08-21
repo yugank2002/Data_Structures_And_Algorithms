67ee07018affab26600448a2b5a4db97ef048755
Yugank Prajapati <135359136+yugank2002@users.noreply.github.com>
2026-08-21 14:07:32 +0530

    Time: 3 ms (95.35%), Space: 21.1 MB (20.16%) - LeetHub

--- 3116-kth-smallest-amount-with-single-denomination-combination/3116-kth-smallest-amount-with-single-denomination-combination.cpp
+++ 3116-kth-smallest-amount-with-single-denomination-combination/3116-kth-smallest-amount-with-single-denomination-combination.cpp
@@ -1,68 +1,64 @@
 class Solution {
 public:
-    long long gcd(long long a, long long b) {
-        if (b == 0) return a;
-        return gcd(b, a % b);
-    }
-
-    long long lcm(long long a, long long b) {
-        return (a / gcd(a, b)) * b;
-    }
-
-    long long count(long long mid, vector<int>& coins) {
-        int n = coins.size();
-        long long pos = 0;
-
-        // All subsets of coins
-        for (int mask = 1; mask < (1 << n); mask++) {
-
-            long long L = 1;
-            int bits = 0;
-            bool valid = true;
-
-            for (int i = 0; i < n; i++) {
-
-                if (mask & (1 << i)) {
-                    bits++;
-
-                    L = lcm(L, coins[i]);
-
-                    // L is already greater than mid
-                    if (L > mid) {
-                        valid = false;
-                        break;
-                    }
+    using ll = long long;
+    long long findKthSmallest(vector<int>& coins, int k) {
+        sort(coins.begin(), coins.end());
+        vector<int> new_coins;
+        for (int x : coins) {
+            bool flag = true;
+            for (int y : new_coins) {
+                if (x % y == 0) {
+                    flag = false;
+                    break;
                 }
             }
+            if (flag) {
+                new_coins.push_back(x);
+            }
+        }
+        coins = new_coins;
 
-            if (!valid) continue;
+        int n = coins.size();
+        int m = (1 << n);
+        vector<int> bit_count(m);
+        vector<ll> lcm(m, 1);
+        ll l = k, r = 1ll * coins[0] * k + 1;
 
-            if (bits % 2 == 1)
-                pos += mid / L;
-            else
-                pos -= mid / L;
+        for (int mask = 1; mask < m; mask++) {
+            int pre_mask = mask & (mask - 1);
+            int i = __builtin_ctz(mask);
+
+            ll tmp = lcm[pre_mask] / gcd(lcm[pre_mask], coins[i]);
+            if (tmp <= r / coins[i]) {
+                lcm[mask] = tmp * coins[i];
+            } else {
+                lcm[mask] = r + 1;
+            }
         }
 
-        return pos;
-    }
+        auto get = [&](ll x) -> ll {
+            ll count = 0;
+            for (int mask = 1; mask < m; mask++) {
+                if (lcm[mask] > x) {
+                    continue;
+                }
+                if (__builtin_popcount(mask) & 1) {
+                    count += x / lcm[mask];
+                } else {
+                    count -= x / lcm[mask];
+                }
+            }
+            return count;
+        };
 
-    long long findKthSmallest(vector<int>& coins, int k) {
-
-        long long start = 1;
-        long long end = LLONG_MAX;
-
-        while (start <= end) {
-
-            long long mid = start + (end - start) / 2;
-
-            long long pos = count(mid, coins);
-
-            if (pos >= k)
-                end = mid - 1;
-            else
-                start = mid + 1;
+        while (l < r) {
+            ll x = (l + r) >> 1;
+            if (get(x) >= k) {
+                r = x;
+            } else {
+                l = x + 1;
+            }
         }
-
-        return start;
+        return l;
     }
 };
