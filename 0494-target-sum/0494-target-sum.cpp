1a1797e134baff36376c815b47cb5efe7b35713a
Yugank Prajapati <135359136+yugank2002@users.noreply.github.com>
2026-08-23 13:43:49 +0530

    Time: 12 ms (53.86%), Space: 25.9 MB (24.97%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,29 +1,34 @@
 class Solution {
 public:
-int total=0;
-int solve(int i, int sum, vector<int>&arr, vector<vector<int>>&memo){
-    if(i<0){
-        if(sum==0)return 1;
-        else return 0;
+    int total = 0;
+    int solve(int i, int sum, vector<int>& arr, vector<vector<int>>& memo) {
+        if (i < 0) {
+            if (sum == 0)
+                return 1;
+            else
+                return 0;
+        }
+        if (sum > total || sum < -total)
+            return 0;
+
+        if (memo[i][sum + total] != -1)
+            return memo[i][sum + total];
+        int plus = solve(i - 1, sum - arr[i], arr, memo);
+        int minus = solve(i - 1, sum + arr[i], arr, memo);
+
+        return memo[i][sum + total] = plus + minus;
     }
-    if (sum > total || sum < -total)
-    return 0;
-
-    if(memo[i][sum+total]!=-1)return memo[i][sum+total];
-    int plus = solve(i-1,sum-arr[i],arr,memo);
-    int minus = solve(i-1,sum+arr[i],arr,memo);
-
-    return memo[i][sum+total] = plus+minus;
-}
     int findTargetSumWays(vector<int>& nums, int target) {
-        
-        for(int elem:nums){
-            total+=elem;
+
+        for (int elem : nums) {
+            total += elem;
         }
 
-        if(abs(target)>total)return 0;
+        if (abs(target) > total)
+            return 0;
 
-        vector<vector<int>>memo(nums.size()+1,vector<int>(total*2+1,-1));
-        return solve(nums.size()-1,target,nums,memo);
+        vector<vector<int>> memo(nums.size() + 1,
+                                 vector<int>(total * 2 + 1, -1));
+        return solve(nums.size() - 1, target, nums, memo);
     }
 };
