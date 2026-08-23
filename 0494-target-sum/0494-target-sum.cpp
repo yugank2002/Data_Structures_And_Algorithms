62429e406c55609a008ab60decd54e4d13e105cc
Yugank Prajapati <135359136+yugank2002@users.noreply.github.com>
2026-08-23 13:43:38 +0530

    Time: 15 ms (50.27%), Space: 26 MB (22.36%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
-int solve(int i, int sum, vector<int>&arr){
+int total=0;
+int solve(int i, int sum, vector<int>&arr, vector<vector<int>>&memo){
     if(i<0){
         if(sum==0)return 1;
         else return 0;
     }
+    if (sum > total || sum < -total)
+    return 0;
 
-    int plus = solve(i-1,sum-arr[i],arr);
-    int minus = solve(i-1,sum+arr[i],arr);
+    if(memo[i][sum+total]!=-1)return memo[i][sum+total];
+    int plus = solve(i-1,sum-arr[i],arr,memo);
+    int minus = solve(i-1,sum+arr[i],arr,memo);
 
-    return plus+minus;
+    return memo[i][sum+total] = plus+minus;
 }
     int findTargetSumWays(vector<int>& nums, int target) {
-        return solve(nums.size()-1,target,nums);
+        
+        for(int elem:nums){
+            total+=elem;
+        }
+
+        if(abs(target)>total)return 0;
+
+        vector<vector<int>>memo(nums.size()+1,vector<int>(total*2+1,-1));
+        return solve(nums.size()-1,target,nums,memo);
     }
 };
