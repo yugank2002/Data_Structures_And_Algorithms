142077b45f348168a1ee7afd00fe44c14969a0fe
Yugank Prajapati <135359136+yugank2002@users.noreply.github.com>
2026-08-24 11:12:09 +0530

    Time: 43 ms (26%), Space: 27.7 MB (17.3%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
+int solve(int i, int j, string& text1, string& text2, vector<vector<int>>&memo){
+    
+    if(i==text1.size() || j==text2.size()){
+        return 0;
+    }
+
+    if(memo[i][j]!=-1)return memo[i][j];
+
+    if(text1[i]==text2[j]){
+        return memo[i][j] = 1+solve(i+1,j+1,text1,text2,memo);
+    }
+
+    
+
+    int skip1 = solve(i+1,j,text1,text2,memo);
+    int skip2 = solve(i,j+1,text1,text2,memo);
+
+    return memo[i][j] = max(skip1,skip2);
+}
     int longestCommonSubsequence(string text1, string text2) {
         int n1 = text1.size();
         int n2 = text2.size();
-        vector<vector<int>> memo(n1 + 1, vector<int>(n2 + 1, 0));
+        vector<vector<int>> memo(n1 + 1, vector<int>(n2 + 1, -1));
 
-        for (int i = 0; i < n1; i++) {
-            memo[i][0] = 0;
-        }
-        for (int j = 0; j < n2; j++) {
-            memo[0][j] = 0;
-        }
-
-        for (int i = 0; i <= n1; i++) {
-            for (int j = 0; j <= n2; j++) {
-                if (i != 0 && j != 0) {
-                    if (text1[i - 1] == text2[j - 1]) {
-                        memo[i][j] = 1 + memo[i - 1][j - 1];
-                    } else {
-                        memo[i][j] = max(memo[i - 1][j], memo[i][j - 1]);
-                    }
-                }
-            }
-        }
-
-        return memo[n1][n2];
+        return solve(0,0,text1,text2,memo);
+        
     }
 };
