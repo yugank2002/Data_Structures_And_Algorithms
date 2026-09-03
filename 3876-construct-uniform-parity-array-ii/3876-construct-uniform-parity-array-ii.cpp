ee9caacc6d099ee90606f41ae01790747f84a330
Yugank Prajapati <135359136+yugank2002@users.noreply.github.com>
2026-09-03 09:48:22 +0530

    Time: 10 ms (42.15%), Space: 165.9 MB (62.83%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -1,48 +1,24 @@
 class Solution {
 public:
-bool checkEven(vector<int>&arr){
-    for(int elem:arr){
-        if(elem%2){
-            return false;
-        }
-    }
-    return true;
-}
-
-bool checkOdd(vector<int>&arr){
-    for(int elem:arr){
-        if(!(elem%2)){
-            return false;
-        }
-    }
-    return true;
-}
     bool uniformArray(vector<int>& nums1) {
-        
-        if(checkEven(nums1)||checkOdd(nums1)){
-            return true;
-        }
 
-        
         int lo = INT_MAX;
         int le = INT_MAX;
-        for(int elem:nums1){
-            if(elem%2){
-                
-                lo = min(lo,elem);
-            }
-            else{
-                
-                le = min(le,elem);
+        for (int elem : nums1) {
+            if (elem % 2) {
+
+                lo = min(lo, elem);
+            } else {
+
+                le = min(le, elem);
             }
         }
 
-       
-        
-        
-        
-        if(le-lo>=1)return true;
-        
+        if (le == INT_MAX || lo == INT_MAX)
+            return true;
+
+        if (le - lo >= 1)
+            return true;
 
         return false;
     }
