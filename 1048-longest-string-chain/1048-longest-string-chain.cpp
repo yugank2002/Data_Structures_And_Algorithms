1924433e916c4f8f501c627830c274017206a034
Yugank Prajapati <135359136+yugank2002@users.noreply.github.com>
2026-09-04 15:47:44 +0530

    Time: 1777 ms (21.61%), Space: 640.4 MB (12.92%) - LeetHub

---
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+bool solve(string str1, string str2){
+    int n1 = str1.size();
+    int n2 = str2.size();
+    if(n2-n1 != 1)return false;
+
+    bool mismatch = false;
+    int i=0;
+    int j=0;
+    while(i<n1 && j<n2){
+        if(str1[i]!=str2[j] && !mismatch){
+            mismatch=true;
+            j++;
+        }
+        else if(str1[i]!=str2[j] && mismatch){
+            return false;
+        }
+        else{
+            i++;
+            j++;
+        }
+        
+    }
+
+    return true;
+}
+    int longestStrChain(vector<string>& words) {
+        sort(words.begin(),words.end(), [](auto &a, auto& b){
+            if(a.size()==b.size()){
+                return a<b;
+            }
+            return a.size()<b.size();
+        });
+        int n = words.size();
+        vector<int>dp(n,1);
+        int maxi = 1;
+        for(int i=1; i<n; i++){
+            for(int j=0; j<i; j++){
+                if(solve(words[j],words[i]) && dp[i]<dp[j]+1){
+                    dp[i] = 1+dp[j];
+                    
+                }
+            }
+            maxi =max(dp[i],maxi);
+        }
+        return maxi;
+    }
+};
