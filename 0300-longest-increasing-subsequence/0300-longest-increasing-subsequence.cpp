1c4ccdf2cc00912f3b4ea7f0fe72f74dfc308766
Yugank Prajapati <135359136+yugank2002@users.noreply.github.com>
2026-09-04 13:30:55 +0530

    Time: 285 ms (31.44%), Space: 296.5 MB (22.03%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
-int solve(int i, int pi, vector<int>&nums, vector<vector<int>>&memo){
-    if(i==nums.size())return 0;
-
-    if(memo[i][pi+1]!=-1)return memo[i][pi+1];
-
-    int skip = solve(i+1,pi,nums,memo);
-
-    int take = 0;
-    if(pi==-1 || nums[pi]<nums[i]){
-        take = 1+solve(i+1,i,nums,memo);
-    }
-
-    return memo[i][pi+1] = max(take,skip);
-}
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>>memo(n,vector<int>(n,-1));
-        return solve(0,-1,nums,memo);
+        vector<vector<int>> memo(n + 1, vector<int>(n + 1, 0));
+        for (int i = n - 1; i >= 0; i--) {
+            for (int pi = i - 1; pi >= -1; pi--) {
+                int skip = memo[i + 1][pi + 1];
+
+                int take = 0;
+                if (pi == -1 || nums[pi] < nums[i]) {
+                    take = 1 + memo[i + 1][i + 1];
+                }
+
+                memo[i][pi + 1] = max(take, skip);
+            }
+        }
+        return memo[0][0];
     }
 };
