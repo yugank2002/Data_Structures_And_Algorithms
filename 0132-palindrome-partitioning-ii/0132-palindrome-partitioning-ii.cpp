4a0df716af930652fab2a8a47722665870a5408f
Yugank Prajapati <135359136+yugank2002@users.noreply.github.com>
2026-09-09 16:00:19 +0530

    Time: 1545 ms (26.49%), Space: 62.7 MB (14.77%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -29,24 +29,22 @@ public:
     }
     int minCut(string s) {
         int n = s.size();
-        vector<vector<int>> memo(n+1, vector<int>(n+1, -1));
-        //return solve(0, n - 1, s, memo);
+        vector<vector<int>> memo(n + 1, vector<int>(n + 1, -1));
+        // return solve(0, n - 1, s, memo);
 
         for (int i = n - 1; i >= 0; i--) {
-            
-                
-                int mini = INT_MAX;
-                for (int k = i; k <n; k++) {
-                    if (isPalindrome(i, k, s)) {
-                        int cut = memo[k + 1][n-1];
-                        mini = min(mini, cut);
-                    }
-                }
 
-                memo[i][n-1] = mini + 1;
-            
+            int mini = INT_MAX;
+            for (int k = i; k < n; k++) {
+                if (isPalindrome(i, k, s)) {
+                    int cut = memo[k + 1][n - 1];
+                    mini = min(mini, cut);
+                }
+            }
+
+            memo[i][n - 1] = mini + 1;
         }
 
-        return memo[0][n-1];
+        return memo[0][n - 1];
     }
 };
