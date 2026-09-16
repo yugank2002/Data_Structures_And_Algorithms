ea051a03b3f9a88f33b4de593d8456621499daf3
Yugank Prajapati <135359136+yugank2002@users.noreply.github.com>
2026-09-16 17:00:09 +0530

    Time: 31 ms (66.36%), Space: 32.2 MB (43.46%) - LeetHub

--- 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.cpp
+++ 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.cpp
@@ -1,31 +1,37 @@
 #define mod 1000000007
-
 class Solution {
 public:
-    int numberOfSets(int n, int k) {
+    int solve(int i, int n, int k) {
+        if (k == 0)
+            return 1;
+        if (i == n)
+            return 0;
 
+        int skip = solve(i + 1, n, k);
+        int take = 0;
+        for (int j = i + 1; j < n; j++) {
+            take += solve(j, n, k - 1);
+        }
+
+        return skip + take;
+    }
+    int numberOfSets(int n, int k) {
+        // return solve(0,n,k);
         vector<vector<int>> memo(n + 1, vector<int>(k + 1, 0));
 
-        for(int i = 0; i <= n; i++) {
+        for (int i = 0; i <= n; i++) {
             memo[i][0] = 1;
         }
 
-        for(int K = 1; K <= k; K++) {
+        for (int K = 1; K <= k; K++) {
+            int take = 0;
+            for (int i = n - 1; i >= 0; i--) {
 
-            long long suffix = 0;
+                int skip = memo[i + 1][K];
+                memo[i][K] = (skip + take)%mod;
 
-            for(int i = n - 1; i >= 0; i--) {
-
-                // Current suffix represents:
-                // memo[i+1][K-1] + ... + memo[n-1][K-1]
-                int take = suffix;
-
-                int skip = memo[i+1][K];
-
-                memo[i][K] = (skip + take) % mod;
-
-                // Prepare suffix for the NEXT i
-                suffix = (suffix + memo[i][K-1]) % mod;
+                take += memo[i][K - 1];
+                take %= mod;
             }
         }
 
