4f3931c94bb994bc0edd208fc20013063ca6084c
Yugank Prajapati <135359136+yugank2002@users.noreply.github.com>
2026-09-20 13:21:59 +0530

    Time: 0 ms (100%), Space: 9.6 MB (78.4%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -4,7 +4,7 @@ public:
         int ans = 0;
         for(int i=1; i<=s.size(); i++){
             char c = s[i-1];
-            ans += (26-(c-'a'))*i;
+            ans += (26-c+'a')*i;
         }
         return ans;
     }
